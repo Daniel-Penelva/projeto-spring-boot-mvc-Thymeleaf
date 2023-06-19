d7b6ce404b32de27a50375cec2517db1353f5bb4
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-19 20:52:02 -0300

    Criando e configurando o projeto

---
+++ springboot/src/main/java/projeto/springboot/controller/IndexController.java
@@ -0,0 +1,10 @@
+package projeto.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	
+	
+
+}
---
+++ springboot/src/test/java/projeto/springboot/ProjetospringbootApplicationTests.java
@@ -0,0 +1,12 @@
+package projeto.springboot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringbootApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
