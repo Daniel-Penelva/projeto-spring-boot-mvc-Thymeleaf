63d882b87a8db9fc15f415ca861ea7de8c4f643e
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-19 23:26:47 -0300

    Criando a PessoaRepository

--- springboot/src/main/java/projeto/springboot/controller/PessoaController.java
+++ springboot/src/main/java/projeto/springboot/controller/PessoaController.java
@@ -1,11 +1,17 @@
 package projeto.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import projeto.springboot.repository.PessoaRepository;
+
 @Controller
 public class PessoaController {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public String inicio() {
