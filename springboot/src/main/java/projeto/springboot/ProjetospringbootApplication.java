cbf59ade3af35ab70eb94d2ba4f0560a349511b2
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-19 22:54:40 -0300

    Testando mapeamento de requisição utilizando o método GET

--- springboot/src/main/java/projeto/springboot/ProjetospringbootApplication.java
+++ springboot/src/main/java/projeto/springboot/ProjetospringbootApplication.java
@@ -3,9 +3,11 @@ package projeto.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "projeto.springboot.model")
+@ComponentScan(basePackages = {"projeto.*"})
 public class ProjetospringbootApplication {
 
 	public static void main(String[] args) {
