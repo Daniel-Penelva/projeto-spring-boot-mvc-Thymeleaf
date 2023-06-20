6aa256c1440d75689d62cab49f9097e79d611db4
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-20 20:45:40 -0300

    Criando método para editar cadastro e corrigindo bug css (faltou a barra)

--- springboot/src/main/java/projeto/springboot/controller/PessoaController.java
+++ springboot/src/main/java/projeto/springboot/controller/PessoaController.java
@@ -1,7 +1,11 @@
 package projeto.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +32,7 @@ public class PessoaController {
 
 		pessoaRepository.save(pessoa);
 
+		// Retorna para a mesma tela, no caso, cadastropessoa
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
 		modelAndView.addObject("pessoas", pessoaIt);
@@ -47,6 +52,20 @@ public class PessoaController {
 		// Add o modelAndView na lista
 		modelAndView.addObject("pessoas", pessoaIt);
 
+		return modelAndView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		
+		// carregar o objeto pessoa através do id buscando o banco de dados
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		
+		// add esse objeto pessoa no modelAndView (na tela)
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		
 		return modelAndView;
 	}
 
