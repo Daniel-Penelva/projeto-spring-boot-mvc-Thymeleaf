c625904330124a73a277716d798519a1a2a15ecd
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-19 22:13:26 -0300

    Criando a entidade Pessoa e add biblioteca javax.xml

--- springboot/src/main/java/projeto/springboot/ProjetospringbootApplication.java
+++ springboot/src/main/java/projeto/springboot/ProjetospringbootApplication.java
@@ -2,8 +2,10 @@ package projeto.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "projeto.springboot.model")
 public class ProjetospringbootApplication {
 
 	public static void main(String[] args) {
