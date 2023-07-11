cfbc45376ebe35b909e52816cb19f1a9ff828978
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-11 03:21:25 -0300

    Criando controle de acessos e perfil (Roles) Spring Security

--- springboot/src/main/java/projeto/springboot/model/Usuario.java
+++ springboot/src/main/java/projeto/springboot/model/Usuario.java
@@ -1,11 +1,16 @@
 package projeto.springboot.model;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,6 +26,12 @@ public class Usuario implements UserDetails {
 
 	private String login;
 	private String senha;
+
+	// Um usuário tem um ou vários acessos.
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuarios_role", joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuário"), 
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role"))
+	private List<Role> roles;
 
 	public Long getId() {
 		return id;
@@ -49,7 +60,7 @@ public class Usuario implements UserDetails {
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 
-		return null;
+		return roles;
 	}
 
 	// Retorna a senha do usuário
