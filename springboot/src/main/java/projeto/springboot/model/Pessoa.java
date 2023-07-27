36f08a9053370d9098d0aaf27922011ddb9da7e3
Daniel Penelva <d4n.andrade@gmail.com>
2023-07-26 21:01:35 -0300

    Criando Upload com Spring - tela e model

--- springboot/src/main/java/projeto/springboot/model/Pessoa.java
+++ springboot/src/main/java/projeto/springboot/model/Pessoa.java
@@ -12,6 +12,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
@@ -65,6 +66,9 @@ public class Pessoa implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
+	
+	@Lob
+	private byte[] curriculo;
 
 	public Long getId() {
 		return id;
@@ -184,6 +188,14 @@ public class Pessoa implements Serializable {
 	
 	public Date getDataNascimento() {
 		return dataNascimento;
+	}
+	
+	public void setCurriculo(byte[] curriculo) {
+		this.curriculo = curriculo;
+	}
+	
+	public byte[] getCurriculo() {
+		return curriculo;
 	}
 
 	@Override
